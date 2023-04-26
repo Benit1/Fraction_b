cb64aa4d9de9e9db8f7311fd8bd6ce3770ed8e12
benit1 <benit1234@gmail.com>
2023-04-26 17:03:47 +0300

    part b

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -3,7 +3,9 @@
 //
 #include "Fraction.hpp"
 
-ariel::Fraction::Fraction(int numerator, int denominator) {
+using namespace ariel;
+
+Fraction::Fraction(int numerator, int denominator) {
     if (denominator == 0) {
         throw std::runtime_error("division by 0");
     }
@@ -12,13 +14,13 @@ ariel::Fraction::Fraction(int numerator, int denominator) {
     minimize();
 }
 
-ariel::Fraction::Fraction() {
+Fraction::Fraction() {
     this->numerator_ = 0;
     this->denominator_ = 1;
 }
 
 //Help function
-void ariel::Fraction::minimize() {
+void Fraction::minimize() {
     int gcd_value = gcd(this->numerator_, this->denominator_);
     this->numerator_ /= gcd_value;
     this->denominator_ /= gcd_value;
@@ -26,7 +28,7 @@ void ariel::Fraction::minimize() {
 }
 
 //Help function(the greatest common divisor)
-int ariel::Fraction::gcd(int numerator, int denominator) {
+int Fraction::gcd(int numerator, int denominator) {
     while (denominator != 0) {
         int temp = denominator;
         denominator = numerator % denominator;
@@ -35,135 +37,145 @@ int ariel::Fraction::gcd(int numerator, int denominator) {
 }
 
 
-ariel::Fraction ariel::Fraction::operator+(const ariel::Fraction &other) const {
-    return other;
+Fraction Fraction::operator+(const Fraction &other) const {
+    //calculate the numerator(by Calculate the numerator of the sum)
+    int numerator_result = (this->numerator_ * other.denominator_) + (this->denominator_ * other.numerator_);
+    //calculate the denominator(by Calculate the denominator of the sum)
+    int denominator_result = (this->denominator_ * other.denominator_);
+    //Create new fraction called sum
+    Fraction sum(numerator_result, denominator_result);
+    //Reduce the value of the fraction
+    sum.minimize();
+    return sum;
+
+
 }
 
-ariel::Fraction ariel::Fraction::operator+(float flo) {
+Fraction Fraction::operator+(float flo) {
     return *this;
 }
 
-ariel::Fraction ariel::operator+(const ariel::Fraction &other, float flo) {
+Fraction operator+(const Fraction &other, float flo) {
     return other;
 }
 
-ariel::Fraction ariel::Fraction::operator-(const ariel::Fraction &other) const {
+Fraction Fraction::operator-(const Fraction &other) const {
     return other;
 }
 
-ariel::Fraction ariel::Fraction::operator-(float flo) {
+Fraction Fraction::operator-(float flo) {
     return *this;
 }
 
-ariel::Fraction ariel::operator-(const ariel::Fraction &other, float flo) {
+Fraction operator-(const Fraction &other, float flo) {
     return other;
 }
 
-ariel::Fraction ariel::Fraction::operator/(const ariel::Fraction &other) const {
+Fraction Fraction::operator/(const Fraction &other) const {
     return other;
 }
 
-ariel::Fraction ariel::Fraction::operator/(float flo) const {
+Fraction Fraction::operator/(float flo) const {
     return *this;
 }
 
-ariel::Fraction ariel::operator/(const ariel::Fraction &other, float flo) {
+Fraction operator/(const Fraction &other, float flo) {
     return other;
 }
 
-ariel::Fraction ariel::Fraction::operator*(const ariel::Fraction &other) const {
+Fraction Fraction::operator*(const Fraction &other) const {
     return other;
 }
 
-ariel::Fraction ariel::Fraction::operator*(float flo) const {
+Fraction Fraction::operator*(float flo) const {
     return *this;
 }
 
-ariel::Fraction ariel::operator*(float flo, const ariel::Fraction &other) {
+Fraction operator*(float flo, const Fraction &other) {
     return other;
 }
 
-ariel::Fraction &ariel::Fraction::operator++() {
+Fraction &Fraction::operator++() {
     return *this;
 }
 
-ariel::Fraction &ariel::Fraction::operator--() {
+Fraction &Fraction::operator--() {
     return *this;
 }
 
-const ariel::Fraction ariel::Fraction::operator++(int) {
+const Fraction Fraction::operator++(int) {
     return *this;
 }
 
-const ariel::Fraction ariel::Fraction::operator--(int) {
+const Fraction Fraction::operator--(int) {
     return *this;
 }
 
-bool ariel::Fraction::operator==(const ariel::Fraction &other) const {
+bool Fraction::operator==(const Fraction &other) const {
     return false;
 }
 
-bool ariel::Fraction::operator==(float flo) const {
+bool Fraction::operator==(float flo) const {
     return false;
 }
 
-bool ariel::operator==(float flo, const ariel::Fraction &other) {
+bool operator==(float flo, const Fraction &other) {
     return false;
 }
 
-bool ariel::Fraction::operator>=(const ariel::Fraction &other) const {
+bool Fraction::operator>=(const Fraction &other) const {
     return false;
 }
 
-bool ariel::Fraction::operator>=(float flo) const {
+bool Fraction::operator>=(float flo) const {
     return false;
 }
 
-bool ariel::operator>=(float flo, const ariel::Fraction &other) {
+bool operator>=(float flo, const Fraction &other) {
     return false;
 }
 
-bool ariel::Fraction::operator<=(const ariel::Fraction &other) const {
+bool Fraction::operator<=(const Fraction &other) const {
     return false;
 }
 
-bool ariel::Fraction::operator<=(float flo) const {
+bool Fraction::operator<=(float flo) const {
     return false;
 }
 
-bool ariel::operator<=(float flo, const ariel::Fraction &other) {
+bool operator<=(float flo, const Fraction &other) {
     return false;
 }
 
-bool ariel::Fraction::operator>(const ariel::Fraction &other) const {
+bool Fraction::operator>(const Fraction &other) const {
     return false;
 }
 
-bool ariel::Fraction::operator>(float flo) const {
+bool Fraction::operator>(float flo) const {
     return false;
 }
 
-bool ariel::operator>(float flo, const ariel::Fraction &other) {
+bool operator>(float flo, const Fraction &other) {
     return false;
 }
 
-bool ariel::Fraction::operator<(const ariel::Fraction &other) const {
+bool Fraction::operator<(const Fraction &other) const {
     return false;
 }
 
-bool ariel::Fraction::operator<(float flo) const {
+bool Fraction::operator<(float flo) const {
     return false;
 }
 
-bool ariel::operator<(float flo, const ariel::Fraction &other) {
+bool operator<(float flo, const Fraction &other) {
     return false;
 }
 
-std::istream &ariel::operator>>(std::istream &inputs, ariel::Fraction &other) {
+std::istream &operator>>(std::istream &inputs, Fraction &other) {
     return inputs;
 }
 
-std::ostream &ariel::operator<<(std::ostream &outputs, const ariel::Fraction &other) {
+std::ostream &operator<<(std::ostream &outputs, const Fraction &other) {
     return outputs;
 }
 
